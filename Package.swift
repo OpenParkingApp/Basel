e09f2ba041fa464917cf6e6dad74c86badf89d08
Kilian Koeltzsch <me@kilian.io>
2019-09-07 18:52:35 +0200

    Move FeedKit back into Basel module

--- Package.swift
+++ Package.swift
@@ -16,13 +16,14 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(path: "../OpenParkingBase"),
         .package(url: "https://github.com/sharplet/Regex", from: "2.1.0"),
+        .package(url: "https://github.com/nmdias/FeedKit", from: "8.1.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "OpenParkingBasel",
-            dependencies: ["OpenParkingBase", "Regex"]),
+            dependencies: ["OpenParkingBase", "FeedKit", "Regex"]),
         .testTarget(
             name: "OpenParkingBaselTests",
             dependencies: ["OpenParkingBasel"]),
